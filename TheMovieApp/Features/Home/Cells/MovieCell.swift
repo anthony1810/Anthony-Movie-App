152f01e2d4ee91a6a456876a0db6cba4b7c99d1a
Anthony Tran <qquang269@gmail.com>
2022-09-11 22:13:44 +0700

    add favorite, price and genre to movie item list

--- TheMovieApp/Features/Home/Cells/MovieCell.swift
+++ TheMovieApp/Features/Home/Cells/MovieCell.swift
@@ -15,7 +15,15 @@ class MovieCell: UICollectionViewCell {
     @IBOutlet weak var imgArtwork: UIImageView!
     @IBOutlet weak var lblTitle: UILabel!
     @IBOutlet weak var lblDesc: UILabel!
-    @IBOutlet weak var btnFavorite: UIButton!
+    @IBOutlet weak var btnFavorite: UIButton! {
+        didSet {
+            btnFavorite.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
+            btnFavorite.tintColor = .systemPink
+        }
+    }
+    @IBOutlet weak var lblPrice: UILabel!
+    @IBOutlet weak var lblGenre: UILabel!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -34,11 +42,14 @@ class MovieCell: UICollectionViewCell {
     func bind(to data: MovieCellModelType) {
         rx.disposeBag.insert(
             data.artworkURL.drive(imgArtwork.rx
-                .imageURL(withPlaceholder: nil,
+                .imageURL(withPlaceholder: UIImage(systemName: "photo"),
                           options: [.transition(.fade(0.25)),
                                     .cacheMemoryOnly])),
             data.name.drive(lblTitle.rx.text),
-            data.desc.drive(lblDesc.rx.text)
+            data.desc.drive(lblDesc.rx.text),
+            data.price.drive(lblPrice.rx.text),
+            data.genre.drive(lblGenre.rx.text),
+            data.isFavorite.map { !$0 }.drive(btnFavorite.rx.isHidden)
         )
     }
 
--- TheMovieApp/Features/Home/Cells/MovieCellModel.swift
+++ TheMovieApp/Features/Home/Cells/MovieCellModel.swift
@@ -12,6 +12,8 @@ protocol MovieCellModelType {
     var artworkURL: Driver<URL?> { get set }
     var name: Driver<String> { get set }
     var desc: Driver<String> { get set }
+    var price: Driver<String> { get set }
+    var genre: Driver<String> { get set }
     var isFavorite: Driver<Bool> { get set }
 }
 
@@ -19,12 +21,16 @@ class MovieCellModel: MovieCellModelType {
     var artworkURL: Driver<URL?>
     var name: Driver<String>
     var desc: Driver<String>
+    var price: Driver<String>
+    var genre: Driver<String>
     var isFavorite: Driver<Bool>
     
     init(with movie: MovieDataType) {
         self.artworkURL = Driver.just(URL(string: movie.artwork.orStringEmpty))
         self.name = Driver.just(movie.name.orStringEmpty)
         self.desc = Driver.just(movie.shortDesc.orStringEmpty)
+        self.price = Driver.just("$\(movie.price.orZero)")
+        self.genre = Driver.just(movie.genre.orStringEmpty)
         self.isFavorite = Driver.just(movie.isFavorite.orFalse)
     }
 }
